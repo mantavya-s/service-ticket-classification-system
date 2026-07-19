7e5618b151c67bc5c69c3772938ae3cde3914c82
Mantavya <themantavyasethi@gmail.com>
2026-07-18 22:09:27 -0400

    created an endpoint for similarity search which runs the retrieval script on the user input

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.schemes import TicketRequest, TicketResponse
+from app.schemes import TicketRequest, TicketResponse, RetrieveRequest
 from app.predict import predict_ticket
+from rag.retrieve import retrieve as similarity_search
 
 app = FastAPI()
 
@@ -18,5 +19,14 @@ async def predict_item(request: TicketRequest):
     return{
         "Category": category,
         "Confidence": confidence
+    }
+
+@app.post("/retrieve/")
+async def retrieve(request: RetrieveRequest):
+    results = similarity_search(request.text, request.limit)
+
+    return{
+        "query": request.text,
+        "results": results
     }
     
